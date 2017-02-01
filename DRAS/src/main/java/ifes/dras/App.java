1c6cb1c3dc066a9479f80b8cb6cc93f3f2dcedc8
Izabely Furtado <izabelyfurtado1@gmail.com>
2017-02-01 14:57:54 -0200

    versao 0
    transformando o projeto em um maven
    configurando o sonar

---
+++ DRAS/src/main/java/ifes/dras/App.java
@@ -0,0 +1,13 @@
+package ifes.dras;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
