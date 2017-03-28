f2a7b5506da0c9fb9de2fbaff110edc0b510dca5
Izabely Furtado <izabelyfurtado1@gmail.com>
2017-03-28 18:58:47 -0300

    versão 5.5
    dados da tela form1

---
+++ dweb/WebContent/assets/js/login.js
@@ -0,0 +1,13 @@
+function login() {
+		var nome = document.getElementById("nome").value;
+		var senha = document.getElementById("senha").value;
+		if (nome == "admin") {
+			if (senha == "admin") {
+				window.location.href = "./cadastroAluno.html"
+			} else {
+				alert("Senha incorreta para este usuário!");
+			}
+		} else {
+			alert("Senha e usuários incorretos!");
+		}
+	}
