96be27bfac3a8b93a32876508cd6de3eebe3bce9
Izabely Furtado <izabelyfurtado1@gmail.com>
2017-04-09 03:55:05 -0300

    versão 10
    pesquisa de alunos feita

---
+++ dweb/WebContent/assets/js/pesquisa.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+function encontraPorLetraI() {
+	nome = "Izabely Almeida Furtado Corrêa";
+	responsavel = "Sérgio Furtado Corrêa";
+	contato = "(27) 99708-8212";
+	nasc = "29/03/1995";
+	turma = "Sonserina"
+    document.getElementById("encontra").innerHTML = "<tr><td>" + nome + "</td><td>" + responsavel + "</td><td>" + contato + "</td><td>" + nasc + "</td><td>" + turma +"</td></tr>";   
+    document.getElementById("encontra").innerHTML += "<tr><td>" + nome + "</td><td>" + responsavel + "</td><td>" + contato + "</td><td>" + nasc + "</td><td>" + turma +"</td></tr>";   
+    document.getElementById("encontra").innerHTML += "<tr><td>" + nome + "</td><td>" + responsavel + "</td><td>" + contato + "</td><td>" + nasc + "</td><td>" + turma +"</td></tr>";   
+}
+
+function encontraPorLetra() {
+	nome = " - ";
+	responsavel = " - ";
+	contato = " - ";
+	nasc = " - ";
+	turma = " - ";
+    document.getElementById("encontra").innerHTML = "<tr><td>" + nome + "</td><td>" + responsavel + "</td><td>" + contato + "</td><td>" + nasc + "</td><td>" + turma +"</td></tr>";   
+    document.getElementById("encontra").innerHTML += "<tr><td>" + nome + "</td><td>" + responsavel + "</td><td>" + contato + "</td><td>" + nasc + "</td><td>" + turma +"</td></tr>";   
+    document.getElementById("encontra").innerHTML += "<tr><td>" + nome + "</td><td>" + responsavel + "</td><td>" + contato + "</td><td>" + nasc + "</td><td>" + turma +"</td></tr>";   
+}
+
+function encontra() {
+	alert("Nenhum Registro Ecnontrado")
+}
